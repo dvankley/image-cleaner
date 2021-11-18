a98c7cb41e54f76e1a385e6195f70ada7d63d504
Dan van Kley <dvankley@gmail.com>
2021-11-17 20:26:03 -0500

    Working on image editor

--- src/main/kotlin/net/djvk/imageCleaner/ui/ParentController.kt
+++ src/main/kotlin/net/djvk/imageCleaner/ui/ParentController.kt
@@ -1,13 +1,21 @@
 package net.djvk.imageCleaner.ui
 
 import javafx.fxml.FXML
-import javafx.scene.control.Button
-import javafx.scene.control.TextField
+import javafx.scene.control.*
+import javafx.scene.image.ImageView
 import javafx.scene.input.MouseEvent
+import javafx.scene.layout.HBox
 import javafx.stage.FileChooser
 import javafx.stage.Stage
 
 class ParentController {
+    @FXML
+    lateinit var tabPane: TabPane
+
+    //region Input Tab
+    @FXML
+    lateinit var tabInput: Tab
+
     @FXML
     lateinit var btnSelectInputDirectory: Button
     @FXML
@@ -17,10 +25,33 @@ class ParentController {
     lateinit var btnSelectWorkingDirectory: Button
     @FXML
     lateinit var txtWorkingDirectory: TextField
+    //endregion
 
-    val stage
+    //region Sample Tab
+    @FXML
+    lateinit var tabSample: Tab
+
+    @FXML
+    lateinit var imgEditor: ImageView
+
+    @FXML
+    lateinit var hboxInputImages: HBox
+    //endregion
+
+    private val stage
         get() = txtInputDirectory.scene.window as Stage
 
+    fun initialize() {
+        tabPane.selectionModel.selectedItemProperty().addListener { value, old, new ->
+            when (new) {
+                tabInput -> {}
+                tabSample -> initSampleTab()
+                else -> throw IllegalArgumentException("Invalid tab $new")
+            }
+        }
+    }
+
+    //region Input Tab
     @FXML
     private fun handleSelectInputDirectoryClick(event: MouseEvent) {
         val path = selectFile("Select Input Directory")
@@ -44,4 +75,32 @@ class ParentController {
         fileChooser.title = title
         return fileChooser.showOpenDialog(stage)?.path
     }
+    //endregion
+
+    //region Sample Tab
+    private fun initSampleTab() {
+        if (txtInputDirectory.text == "" || txtWorkingDirectory.text == "") {
+            val alert = Alert(Alert.AlertType.ERROR, "Input directory selection required")
+            alert.showAndWait()
+        }
+    }
+
+    @FXML
+    private fun editorPress(event: MouseEvent) {
+        val path = selectFile("Select Working Directory")
+
+        if (path != null) {
+            txtWorkingDirectory.text = path
+        }
+    }
+
+    @FXML
+    private fun editorRelease(event: MouseEvent) {
+        val path = selectFile("Select Working Directory")
+
+        if (path != null) {
+            txtWorkingDirectory.text = path
+        }
+    }
+    //endregion
 }
